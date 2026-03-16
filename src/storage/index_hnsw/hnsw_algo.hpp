ec6ea300df0ccb7437c3a3c5190fea5c739ad1ff
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2026-03-16 16:08:51 +0900

    update

--- src/storage/index_hnsw/hnsw_algo.hpp
+++ src/storage/index_hnsw/hnsw_algo.hpp
@@ -374,7 +374,7 @@ namespace cubhnsw
     std::size_t expansion_size = std::max (k, expansion);
 
     // pre-reserve for visits
-    context.m_visits.reserve (expansion_size);
+    context.m_visits.reserve (expansion_size * 8);
 
     // pre-reserve for top and next
     if (!top.reserve (expansion_size) || !next.reserve (expansion_size))
